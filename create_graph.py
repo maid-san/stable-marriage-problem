175cdf8e3c9454504e05391f22ffafafa4dd1d23
homomaid <canon.xex@gmail.com>
2019-10-07 02:24:33 +0900

    Implement constract_graph()

--- create_graph.py
+++ create_graph.py
@@ -7,24 +7,23 @@ import pprint
 class GraphSolver:
     def __init__ (self, pref):
         self.preference = pref
-        self.size = len(self.preference[0][0])
+        self.size = len(pref[0][0])
 
-    def create_graph(self, list):
+    def create_graph(self, pref):
         """
         create Stable Marriage Problem Graph from preference list
         """
-
         ret = nx.DiGraph()
 
         # Add edge horizontal(man) direction
-        for number, man_row in enumerate(list[0]):
+        for number, man_row in enumerate(pref[0]):
             for i, rank_i in enumerate(man_row):
                 for j, rank_j in enumerate(man_row):
                     if rank_i > rank_j:
                         ret.add_edge(number * self.size + i, number * self.size + j)
 
         # Add edge vertical(woman) direction
-        for number, woman_row in enumerate(list[1]):
+        for number, woman_row in enumerate(pref[1]):
             for i, rank_i in enumerate(woman_row):
                 for j, rank_j in enumerate(woman_row):
                     if rank_i > rank_j:
@@ -32,14 +31,13 @@ class GraphSolver:
 
         return ret
 
-    def create_subgraph(self, list, graph):
+    def create_subgraph(self, pref, graph):
         """
         create subgraph(only woman's best and next good point) from master graph
         """
-
         ret = copy.deepcopy(graph)
 
-        for number, woman_row in enumerate(list[1]):
+        for number, woman_row in enumerate(pref[1]):
             for i, rank in enumerate(woman_row):
                 if not math.isnan(rank):
                     if rank != 1 and rank != 2:
@@ -47,52 +45,67 @@ class GraphSolver:
 
         return ret
 
-    def get_woman_best_point(self, list):
+    def get_man_best_point(self, pref):
         ret = []
 
-        for number, woman_row in enumerate(list[1]):
-            for i, rank in enumerate(woman_row):
-                if rank == 1:
-                    ret.append(number + i * self.size)
-
-        return ret
-
-    def get_man_best_point(self, list):
-        ret = []
-
-        for number, man_row in enumerate(list[0]):
+        for number, man_row in enumerate(pref[0]):
             for i, rank in enumerate(man_row):
                 if rank == 1:
                     ret.append(number * self.size + i)
 
         return ret
 
+    def get_woman_best_point(self, pref):
+        ret = []
+
+        for number, woman_row in enumerate(pref[1]):
+            for i, rank in enumerate(woman_row):
+                if rank == 1:
+                    ret.append(number + i * self.size)
+
+        return ret
+
     def contract_graph (self, graph, man_best_point, woman_best_point):
+        ret = copy.deepcopy(graph)
+        delete = []
 
+        for m in man_best_point:
+            for pre in list(graph.predecessors(m)):
+                if m % self.size == pre % self.size:
+                    delete.append(pre)
 
-        return graph
+        for w in woman_best_point:
+            for pre in list(graph.predecessors(w)):
+                if w / self.size == pre / self.size:
+                    delete.append(pre)
 
-    def reflesh_preference(self, preference, delete):
-        ret = copy.deepcopy(preference)
+        delete = list(set(delete))
+        for d in delete:
+            ret.remove_node(d)
+
+        return ret, delete
+
+    def reflesh_preference(self, pref, delete):
+        ret = copy.deepcopy(pref)
 
         for d in delete:
-            d_i = d / 4
-            d_j = d % 4
+            d_i = d / self.size
+            d_j = d % self.size
 
             for i, man_row in enumerate(ret[0]):
                 if i == d_i:
                     for j, rank in enumerate(man_row):
                         if j == d_j:
                             ret[0][i][j] = float('nan')
-                        elif rank > ret[0][d_i][d_j]:
+                        elif rank > pref[0][d_i][d_j]:
                             ret[0][i][j] -= 1
 
             for i, woman_row in enumerate(ret[1]):
-                for j, _ in enumerate(woman_row):
-                    if j == d_i:
-                        if ret[1][i][j] == 1:
+                if i == d_j:
+                    for j, rank in enumerate(woman_row):
+                        if j == d_i:
                             ret[1][i][j] = float('nan')
-                        else:
+                        elif rank > pref[1][d_j][d_i]:
                             ret[1][i][j] -= 1
 
         return ret
@@ -101,34 +114,36 @@ class GraphSolver:
         pref = copy.deepcopy(self.preference)
         i = 1
 
-        while True:
-            graph = self.create_graph(pref)
-            subgraph = self.create_subgraph(pref, graph)
-            cycles = list(nx.simple_cycles(subgraph))
+        _, delete_point = self.contract_graph(self.create_graph(pref), self.get_man_best_point(pref), self.get_woman_best_point(pref))
+        pref = self.reflesh_preference(pref, delete_point)
 
-            # print("cycles: " + str(cycles))
+        while True:
+            # print("[Debug] pref: " + str(pref))
+            graph    = self.create_graph(pref)
+            subgraph = self.create_subgraph(pref, graph)
+            # print("[Debug] graph: "    + str(list(graph.nodes())))
+            # print("[Debug] subgraph: " + str(list(subgraph.nodes())))
+            cycles = list(nx.simple_cycles(subgraph))
+            # print("[Debug] cycles: " + str(cycles))
             if len(cycles) == 0:
                 break
-
-            woman_best_point = self.get_woman_best_point(pref)
-            delete_point = list(set(cycles[0]) & set(woman_best_point))
+            delete_point = list(set(cycles[0]) & set(self.get_woman_best_point(pref)))
             pref = self.reflesh_preference(pref, delete_point)
 
             i += len(cycles)
         return i
 
+# Test Code
 pref = [
             [
-                [1, 2, 3, 4],
-                [2, 1, 4, 3],
-                [3, 4, 1, 2],
-                [4, 3, 2, 1]
+                [1, 2, 3],
+                [3, 1, 2],
+                [3, 2, 1]
             ],
             [
-                [4, 3, 2, 1],
-                [3, 4, 1, 2],
-                [2, 1, 4, 3],
-                [1, 2, 3, 4]
+                [3, 1, 2],
+                [1, 3, 2],
+                [3, 1, 2]
             ]
         ]
 
